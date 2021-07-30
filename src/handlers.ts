a8290bf425097537a2029eaa4e94997182a8a5b8
MediaURL <contact@mediaurl.io>
2021-07-30 21:38:16 +0200

    Fixed client side bug

--- src/handlers.ts
+++ src/handlers.ts
@@ -26,47 +26,10 @@ export const catalogHandler: ActionHandlers["catalog"] = async (input, ctx) => {
         return parseList(await resp.text());
     });
 
+    console.log("XXX", url, results.length, cursor);
+
     return {
-        nextCursor: results.length ? cursor + 1 : null,
-        features: {
-            filter: [
-                {
-                    id: "language",
-                    name: "Language",
-                    values: [
-                        { value: "English", key: "en" },
-                        { value: "Español", key: "es" },
-                        { value: "日本語", key: "ja" },
-                        { value: "Português brasileiro", key: "pt-br" },
-                        { value: "中文 (繁體)", key: "zh-tw" },
-                        { value: "한국어", key: "ko" },
-                    ],
-                },
-                {
-                    id: "duration",
-                    name: "Duration",
-                    values: [
-                        { value: "0–6 minutes", key: "0-6" },
-                        { value: "6–12 minutes", key: "6-12" },
-                        { value: "12–18 minutes", key: "12-18" },
-                        { value: "18+ minutes", key: "18+" },
-                    ],
-                },
-                {
-                    id: "topics",
-                    name: "Topics",
-                    multiselect: true,
-                    values: [
-                        { value: "Technology", key: "Technology" },
-                        { value: "Entertainment", key: "Entertainment" },
-                        { value: "Design", key: "Design" },
-                        { value: "Business", key: "Business" },
-                        { value: "Science", key: "Science" },
-                        { value: "Global issues", key: "Global issues" },
-                    ],
-                },
-            ],
-        },
+        nextCursor: results.length >= 36 ? cursor + 1 : null,
         items: results.map((item) => {
             const id = item.link;
             return {
--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,43 @@ export const tedAddon = createAddon({
         {
             features: {
                 search: { enabled: true },
+                filter: [
+                    {
+                        id: "language",
+                        name: "Language",
+                        values: [
+                            { value: "English", key: "en" },
+                            { value: "Español", key: "es" },
+                            { value: "日本語", key: "ja" },
+                            { value: "Português brasileiro", key: "pt-br" },
+                            { value: "中文 (繁體)", key: "zh-tw" },
+                            { value: "한국어", key: "ko" },
+                        ],
+                    },
+                    {
+                        id: "duration",
+                        name: "Duration",
+                        values: [
+                            { value: "0–6 minutes", key: "0-6" },
+                            { value: "6–12 minutes", key: "6-12" },
+                            { value: "12–18 minutes", key: "12-18" },
+                            { value: "18+ minutes", key: "18+" },
+                        ],
+                    },
+                    {
+                        id: "topics",
+                        name: "Topics",
+                        multiselect: true,
+                        values: [
+                            { value: "Technology", key: "Technology" },
+                            { value: "Entertainment", key: "Entertainment" },
+                            { value: "Design", key: "Design" },
+                            { value: "Business", key: "Business" },
+                            { value: "Science", key: "Science" },
+                            { value: "Global issues", key: "Global issues" },
+                        ],
+                    },
+                ],
             },
             options: {
                 imageShape: "landscape",
